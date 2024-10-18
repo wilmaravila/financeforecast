b42614adfb7337437fbd0a35c27ee822b7cae0ed
wilmaravila <wilmarhelyan@gmail.com>
2024-10-17 21:21:10 -0500

    agregacion de frecha de creacion a usuarios

--- DB_financeforecast/financeforecast.sql
+++ DB_financeforecast/financeforecast.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-10-2024 a las 04:00:23
+-- Tiempo de generación: 18-10-2024 a las 04:19:11
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -46,7 +46,84 @@ INSERT INTO `credenciales` (`id`, `correo`, `usuario_id`, `password`) VALUES
 (5, 'luis.fernandez@example.com', 5, 'passL@456'),
 (6, 'Wil@gmail.com', 6, 'Wa123.'),
 (11, 'tatorres2005@gmail.com', 7, 'aqW23.Tt'),
-(12, 'wavilaga@uninpahu.edu.co', 8, 'dffdf12L1.');
+(12, 'wavilaga@uninpahu.edu.co', 8, 'dffdf12L1.'),
+(13, 'sdfsdfsdafafda', 9, 'ddd12S.j'),
+(14, 'tatorres2005@gmail.com', 10, '12Aw..12');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entidad_bancaria`
+--
+
+CREATE TABLE `entidad_bancaria` (
+  `id_banco` int(11) NOT NULL,
+  `banco_nombre` varchar(100) DEFAULT NULL,
+  `banco_pais` varchar(50) DEFAULT NULL,
+  `calificacion` varchar(50) DEFAULT NULL,
+  `inf_adicional` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto_financiero`
+--
+
+CREATE TABLE `producto_financiero` (
+  `id_producto` int(11) NOT NULL,
+  `prd_tipo` varchar(50) DEFAULT NULL,
+  `tasa_interes` double DEFAULT NULL,
+  `entidad_bancaria` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `terminos_condiciones` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recomendacion_inv`
+--
+
+CREATE TABLE `recomendacion_inv` (
+  `id_recomendacion` int(11) NOT NULL,
+  `inv_tipo` varchar(50) DEFAULT NULL,
+  `inv_riesgo` varchar(50) DEFAULT NULL,
+  `inv_plazo` varchar(50) DEFAULT NULL,
+  `retorno_estimado` double DEFAULT NULL,
+  `inv_fecha` datetime DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro_financiero`
+--
+
+CREATE TABLE `registro_financiero` (
+  `id_registro` int(11) NOT NULL,
+  `rf_tipo` varchar(50) DEFAULT NULL,
+  `rf_categoria` varchar(50) DEFAULT NULL,
+  `rf_monto` double DEFAULT NULL,
+  `rf_fecha` datetime DEFAULT NULL,
+  `re_descripcion` text DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tendencia_mercado`
+--
+
+CREATE TABLE `tendencia_mercado` (
+  `id_tendencia` int(11) NOT NULL,
+  `ted_tipo` varchar(50) DEFAULT NULL,
+  `ted_analisis` text DEFAULT NULL,
+  `ted_fecha` datetime DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -56,6 +133,7 @@ INSERT INTO `credenciales` (`id`, `correo`, `usuario_id`, `password`) VALUES
 
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
+  `fecha_registro` date NOT NULL DEFAULT current_timestamp(),
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
   `fecha_nacimiento` date NOT NULL
@@ -65,15 +143,17 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `fecha_nacimiento`) VALUES
-(1, 'Juan', 'Pérez', '1990-05-15'),
-(2, 'María', 'Gómez', '1985-11-20'),
-(3, 'Carlos', 'López', '1992-02-10'),
-(4, 'Ana', 'Martínez', '1988-09-30'),
-(5, 'Luis', 'Fernández', '1995-07-12'),
-(6, 'Wilmar', 'Avila', '2003-10-24'),
-(7, 'aass', 'eeeee', '2024-10-09'),
-(8, 'f', 'd', '2024-10-09');
+INSERT INTO `usuarios` (`id_usuario`, `fecha_registro`, `nombre`, `apellido`, `fecha_nacimiento`) VALUES
+(1, '2023-01-01', 'Juan', 'Pérez', '1990-05-15'),
+(2, '2023-01-01', 'María', 'Gómez', '1985-11-20'),
+(3, '2023-01-01', 'Carlos', 'López', '1992-02-10'),
+(4, '2023-01-01', 'Ana', 'Martínez', '1988-09-30'),
+(5, '2023-01-01', 'Luis', 'Fernández', '1995-07-12'),
+(6, '2023-01-01', 'Wilmar', 'Avila', '2003-10-24'),
+(7, '2023-01-01', 'aass', 'eeeee', '2024-10-09'),
+(8, '2023-01-01', 'f', 'd', '2024-10-09'),
+(9, '2023-01-01', 'w', 't', '2024-10-15'),
+(10, '2024-10-17', 'q', 'w', '2024-10-10');
 
 --
 -- Índices para tablas volcadas
@@ -85,6 +165,41 @@ INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `fecha_nacimiento`) 
 ALTER TABLE `credenciales`
   ADD PRIMARY KEY (`id`),
   ADD KEY `persona_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `entidad_bancaria`
+--
+ALTER TABLE `entidad_bancaria`
+  ADD PRIMARY KEY (`id_banco`);
+
+--
+-- Indices de la tabla `producto_financiero`
+--
+ALTER TABLE `producto_financiero`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `entidad_bancaria` (`entidad_bancaria`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `recomendacion_inv`
+--
+ALTER TABLE `recomendacion_inv`
+  ADD PRIMARY KEY (`id_recomendacion`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `registro_financiero`
+--
+ALTER TABLE `registro_financiero`
+  ADD PRIMARY KEY (`id_registro`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `tendencia_mercado`
+--
+ALTER TABLE `tendencia_mercado`
+  ADD PRIMARY KEY (`id_tendencia`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -100,13 +215,43 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `credenciales`
 --
 ALTER TABLE `credenciales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `entidad_bancaria`
+--
+ALTER TABLE `entidad_bancaria`
+  MODIFY `id_banco` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto_financiero`
+--
+ALTER TABLE `producto_financiero`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `recomendacion_inv`
+--
+ALTER TABLE `recomendacion_inv`
+  MODIFY `id_recomendacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `registro_financiero`
+--
+ALTER TABLE `registro_financiero`
+  MODIFY `id_registro` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tendencia_mercado`
+--
+ALTER TABLE `tendencia_mercado`
+  MODIFY `id_tendencia` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
@@ -117,6 +262,31 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `credenciales`
   ADD CONSTRAINT `credenciales_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `producto_financiero`
+--
+ALTER TABLE `producto_financiero`
+  ADD CONSTRAINT `producto_financiero_ibfk_1` FOREIGN KEY (`entidad_bancaria`) REFERENCES `entidad_bancaria` (`id_banco`) ON DELETE SET NULL,
+  ADD CONSTRAINT `producto_financiero_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `recomendacion_inv`
+--
+ALTER TABLE `recomendacion_inv`
+  ADD CONSTRAINT `recomendacion_inv_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `registro_financiero`
+--
+ALTER TABLE `registro_financiero`
+  ADD CONSTRAINT `registro_financiero_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `tendencia_mercado`
+--
+ALTER TABLE `tendencia_mercado`
+  ADD CONSTRAINT `tendencia_mercado_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
