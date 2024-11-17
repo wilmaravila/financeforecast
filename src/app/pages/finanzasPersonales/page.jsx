c2b8c39fd6bad9e3a74116aa9e9fb80f83076352
Samyr <samyrbd15120@outlook.com>
2024-11-16 21:03:14 -0500

    finanzas personales final

--- src/app/pages/finanzasPersonales/page.jsx
+++ src/app/pages/finanzasPersonales/page.jsx
@@ -1,13 +1,23 @@
+"use client";
 import Navegar from './LayoutF';
 import React from 'react';
 import Image from 'next/image';
 import jubilacion from '../../assets/images/jubilacion.jpg';
 import regla from '../../assets/images/regla72.jpeg';
 import inv from '../../assets/images/inversion2.png';
+import { useState } from 'react';
 import { Button } from '../../Button';
 
 
 export default  function finanzasPersonales() {
+  const [showInvestmentDetails, setShowInvestmentDetails] = useState(false);
+  const [showRetirementDetails, setShowRetirementDetails] = useState(false);
+  const [showRuleDetails, setShowRuleDetails] = useState(false);
+
+  const toggleInvestmentDetails = () => setShowInvestmentDetails(!showInvestmentDetails);
+  const toggleRetirementDetails = () => setShowRetirementDetails(!showRetirementDetails);
+  const toggleRuleDetails = () => setShowRuleDetails(!showRuleDetails);
+  
   return (
     <>
     <Navegar></Navegar>
@@ -34,7 +44,9 @@ export default  function finanzasPersonales() {
           </div>
           <h3 className="font-semibold mb-2 mt-10">Inversiones Éticas: Cómo Hacer Que tu Dinero Trabaje...</h3>
           <p className="text-sm mb-4">En un mundo cada vez más consciente de los desafíos sociales y ambientales, las inversiones éticas han emergido como una poderosa herramienta para generar un impacto positivo, permitiendo que el capital no solo genere rendimientos financieros...</p>
-          <a href="../" className="text-x1 font-semibold text-gray-600 hover:text-cyan-600">Ver Más -</a>
+          <a href="#" onClick={toggleInvestmentDetails} className="text-x1 font-semibold text-gray-600 hover:text-cyan-600">
+            {showInvestmentDetails ? 'Ver Menos' : 'Ver Más -'}
+          </a>
         </div>
         <div className="bg-gray-200 p-5 rounded-[20px] min-h-[400px] w-[780px] flex flex-col">
           <div className="bg-gray-300 rounded-[10px] h-80">
@@ -62,8 +74,79 @@ export default  function finanzasPersonales() {
           </div>
 
     </div>
+  </section>
 
-    </section>
+    {showInvestmentDetails && (
+      <section className="flex justify-center h-screen bg-gray-200 rounded-[20px] mb-10">
+        <div className="bg-gray-200 p-5 rounded-[20px]">
+          <p className="text-blue text-2xl font-bold mb-2 mt-">Inversiones Éticas: cómo hacer que tu dinero trabaje para el bien</p>
+          <p className="mb-4">
+            En un mundo cada vez más consciente de los desafíos sociales y ambientales, 
+            las inversiones éticas han emergido como una poderosa herramienta
+            para aquellos que desean que su dinero no solo genere rendimientos financieros, 
+            sino que también tenga un impacto positivo en la sociedad. 
+            Este tipo de inversiones no se enfocan únicamente en el rendimiento económico, sino 
+            también en promover valores como la justicia social, la sostenibilidad ambiental y la responsabilidad corporativa.
+          </p>
+          <p className="text-blue text-2xl font-bold mb-2 mt-4">¿Qué son las inversiones éticas?</p>
+          <p className="mb-4">
+            Las inversiones éticas, también conocidas como inversiones 
+            socialmente responsables (ISR) o inversiones sostenibles, 
+            se refieren a la práctica de elegir inversiones basadas en criterios éticos y 
+            morales, además de los tradicionales criterios financieros. Este enfoque considera 
+            ambientales, sociales y de gobernanza (ESG, por sus siglas en inglés) 
+            al evaluar las oportunidades de inversión. La idea es apoyar a empresas y proyectos que 
+            actúen de manera responsable y que contribuyan positivamente al bienestar global.
+          </p>
+          <p className="text-blue text-2xl font-bold mb-2 mt-4">Su impacto en el medioambiente: ¿realizan prácticas sostenibles?</p>
+          <p className="mb-4">
+            Estas inversiones se dirigen a empresas que adoptan prácticas sostenibles, 
+            como la reducción de emisiones de carbono, la gestión adecuada de los recursos 
+            naturales y la minimización del impacto ambiental. Se trataría de, por ejemplo, 
+            los sectores de energías renovables, tecnologías limpias o de conservación. 
+            ¿No los conoces? Te los desarrollamos a continuación:
+          </p>
+          <p className="text-blue text-2xl font-bold mb-2 mt-4">Energías renovables: qué es y por qué es importante</p>
+          <p className="mb-4">
+            El sector de las energías renovables es un pilar fundamental en las inversiones 
+            éticas debido a su papel crucial en la mitigación del cambio climático y la 
+            promoción de la sostenibilidad ambiental. Empresas dedicadas a la energía solar, 
+            eólica y otras fuentes limpias reducen la dependencia de combustibles fósiles, disminuyendo 
+            las emisiones de gases de efecto invernadero.
+          </p>
+          <p className="text-blue text-2xl font-bold mb-2 mt-4">Tecnologías limpias: qué son y por qué el mundo las necesita</p>
+          <p className="mb-2">
+            El sector de las tecnologías limpias engloba todas las innovaciones que mejoran la 
+            eficiencia y reducen el impacto ambiental de procesos industriales y cotidianos.
+          </p>
+          <p className="mb-2">
+            Este sector empresarial comprende vehículos eléctricos, sistemas de 
+            reciclaje avanzado, edificios inteligentes, mejoras en aislantes térmicos, lámparas de 
+            bajo consumo y todo tipo de materiales que garanticen un equilibrio entre el progreso 
+            tecnológico que no deja de progresar y la protección del medio ambiente.
+          </p>
+          <p className="text-blue text-2xl font-bold mb-2 mt-4">Sector de conservación, un pilar esencial para el futuro del planeta</p>
+          <p className="mb-2">
+            Por otro lado, el sector de la conservación se centra en proteger y restaurar los ecosistemas 
+            naturales, preservando la biodiversidad y los recursos del planeta.
+          </p>
+          <p className='mb-2'>
+            Algunos de los proyectos en este sector trabajan en la reforestación, la protección de hábitats 
+            críticos y la gestión sostenible de tierras y aguas. Estos trabajos no solo combaten el cambio 
+            climático al capturar carbono, sino que también aseguran la supervivencia de especies en peligro
+            de extinción y mejoran la calidad de vida de las comunidades locales.
+          </p>
+          <p className="text-blue text-2xl font-bold mb-2 mt-4">Su impacto en el conjunto de la sociedad: ¿buscan mejorarla?</p>
+          <p className='mb-4'>
+            Por ejemplo, una de las prácticas más representativas y comunes entre las empresas es la equidad 
+            salarial, es decir, que la organización implemente una política de remuneración justa e igualitaria 
+            para todos los grupos que forman parte de la organización. O, por ejemplo, también te puedes fijar 
+            en si la empresa pone en marcha políticas de diversidad e inclusión social para integrar en su plantilla a 
+            personas con discapacidad, en riesgo de exclusión social o que formen parte de colectivos vulnerables.
+          </p>
+        </div>
+      </section>
+    )}
 
     <section className="mb-12">
     <h2 className="text-2xl font-bold mb-4">Maneja tus Finanzas</h2>
@@ -111,4 +194,3 @@ export default  function finanzasPersonales() {
     </>
     )
 }
-
